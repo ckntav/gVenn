70d8711d45f324a07ad0fbab968a9668e0ceb798
Christophe Tav <christophe.tav@gmail.com>
2025-08-24 10:51:18 -0400

    add zzz to load today's date

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+#' Today's Date at Package Load Time
+#'
+#' This variable stores the current date (in "yyyymmdd" format) at the time the
+#' package is loaded. It is useful for reproducible filenames (e.g., in
+#' `saveViz()`), and is automatically set when the package is attached.
+#'
+#' @format A character string (e.g., "20250624").
+#' @export
+today <- NULL
+
+get_today <- function() {
+    gsub("-", "", as.character(lubridate::today()))
+}
+
+.onLoad <- function(libname, pkgname) {
+    # Assign to the namespace (for internal access)
+    ns <- asNamespace(pkgname)
+    assign("today", get_today(), envir = ns)
+
+    # Assign to the package environment (for user access)
+    assign("today", get_today(), envir = parent.env(environment()))
+}

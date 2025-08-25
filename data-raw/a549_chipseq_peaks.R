4cc70e0a30c0c03297e66047bfd11ce3beada02a
Christophe Tav <christophe.tav@gmail.com>
2025-08-25 16:18:28 -0400

    add example dataset: a549_chipseq_peaks and gene_list

--- data-raw/a549_chipseq_peaks.R
+++ data-raw/a549_chipseq_peaks.R
@@ -10,9 +10,9 @@ bed_med1 <- system.file("extdata", "A549_MED1_Dex.stdchr.bed.gz", package = "gVe
 bed_brd4 <- system.file("extdata", "A549_BRD4_Dex.stdchr.bed.gz", package = "gVenn")
 bed_gr   <- system.file("extdata", "A549_GR_Dex.stdchr.bed.gz", package = "gVenn")
 
-# Helper function: keep only peaks on chr8 (or "8" depending on genome build)
-keep_chr8 <- function(gr) {
-    gr[seqnames(gr) %in% c("chr8", "8")]
+# Helper function: keep only peaks on chr7 (or "8" depending on genome build)
+keep_chr7 <- function(gr) {
+    gr[seqnames(gr) %in% c("chr7", "7")]
 }
 
 # Import full consensus peak sets
@@ -20,15 +20,15 @@ med1_full <- rtracklayer::import(bed_med1)
 brd4_full <- rtracklayer::import(bed_brd4)
 gr_full   <- rtracklayer::import(bed_gr)
 
-# Subset each peak set to chr8 only (keeps the dataset small and fast for examples)
-med1_chr8 <- keep_chr8(med1_full)
-brd4_chr8 <- keep_chr8(brd4_full)
-gr_chr8 <- keep_chr8(gr_full)
+# Subset each peak set to chr7 only (keeps the dataset small and fast for examples)
+med1_chr7 <- keep_chr7(med1_full)
+brd4_chr7 <- keep_chr7(brd4_full)
+gr_chr7 <- keep_chr7(gr_full)
 
 # Combine into a GRangesList with descriptive names
-a549_chipseq_peaks <- GRangesList("MED1_Dex_chr8" = med1_chr8,
-                                  "BRD4_Dex_chr8" = brd4_chr8,
-                                  "GR_Dex_chr8" = gr_chr8)
+a549_chipseq_peaks <- GRangesList("MED1_Dex_chr7" = med1_chr7,
+                                  "BRD4_Dex_chr7" = brd4_chr7,
+                                  "GR_Dex_chr7" = gr_chr7)
 
 # Save the dataset into data/a549_chipseq_peaks.rda
 #   - overwrite = TRUE allows updating
